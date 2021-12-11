9f3c8f24aadde42f2744ba985154b87bd484e37f
ConorHK <conorknowles@gmail.com>
2021-12-11 14:54:44 +0000

    script to add pth file to user sites as repo structure isnt package friendly

--- path.sh
+++ path.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-export PYTHONPATH=$PYTHONPATH:$(pwd)
+SITEDIR=$(python -m site --user-site)
+
+# create if it doesn't exist
+mkdir -p "$SITEDIR"
+
+# create new .pth file with our path
+echo "$(pwd)" > "$SITEDIR/aoc.pth"
