ca705f4540d8b4536643ee9aee6495d4d0faae42
ConorHK <conorknowles@gmail.com>
2021-12-11 13:23:14 +0000

    dayeleven

--- 2021/dayeleven/answer.py
+++ 2021/dayeleven/answer.py
@@ -1,12 +1,14 @@
 import numpy as np
 
+FLASHED = -1
+READY_TO_FLASH = 10
+SIMULTANEOUS_FLASH = 0
+START_ENERGY = 0
+
 with open("input.txt") as f:
     grid = np.array(
         [[int(number) for number in line.strip()] for line in f.readlines()]
     )
-
-
-flashes = 0
 
 
 def surrounding_rows(position):
@@ -20,26 +22,29 @@ def surrounding_rows(position):
 def flash(x, y):
     global flashes
     flashes += 1
-    grid[x, y] = -1
+    grid[x, y] = FLASHED
     for row in surrounding_rows(x):
         for column in surrounding_rows(y):
-            if grid[row, column] != -1:
+            if grid[row, column] != FLASHED:
                 grid[row, column] += 1
-                if grid[row, column] >= 10:
+                if grid[row, column] >= READY_TO_FLASH:
                     flash(x=row, y=column)
+
 
 def run_simulation(iterations):
     global grid
     for iteration in range(iterations):
-        grid = np.where(grid == -1, 0, grid)
-        if np.sum(grid) == 0:
+        grid = np.where(grid == FLASHED, START_ENERGY, grid)
+        if np.sum(grid) == SIMULTANEOUS_FLASH:
             return iteration
         grid = grid + 1
         for (x, y), octopus in np.ndenumerate(grid):
-            if octopus >= 10:
+            if octopus >= READY_TO_FLASH:
                 flash(x=x, y=y)
     return flashes
 
+
+flashes = 0
 grid_copy = grid.copy()
 print(run_simulation(iterations=100))  # part one
 grid = grid_copy
